ea4cbcb8ba8735b70b1d37025f4b058afc720fa9
Yapnounkie <edmundopierre@yahoo.com>
2021-02-10 14:34:54 -0800

    fix minor errors

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,19 @@
 // Code EyesOnMe Component Here
+
+import React, {Component} from 'react'
+
+export default class EyesOnMe extends Component() {
+    //focus when your write on it
+    handleFocus=()=> console.log('Good!')
+   //blur when elements lost his focus
+    handleBlur=()=>console.log('Hey! Eyes on me!')
+    
+    render() {
+        return (
+            <div>
+                <button onFocus={this.handleFocus} onBlur={this.handleBlur}>Eyes</button>
+            </div>  
+        )
+    }
+}
+
